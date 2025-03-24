ba6fe44904162ffa184fa01c40880b44d472631f
czurmuhl <czurmuhl@gmail.com>
2025-03-23 23:38:08 -0400

    github pages ready

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  basePath: '/whod-you-rather', // replace with your GitHub repository name
+  trailingSlash: true,     // Ensure trailing slashes for GitHub Pages to work correctly
+
 };
 
 export default nextConfig;
