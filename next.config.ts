913fb069a472903efd3e20b1b8377d002df34f6a
czurmuhl <czurmuhl@gmail.com>
2025-03-24 00:51:23 -0400

    update enxt config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV == "production"
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 
-  basePath: isProd ? '/whod-you-rather' : "", // replace with your GitHub repository name
-  assetPrefix: isProd ? '/whod-you-rather' : "", // Same as your basePath
+  basePath: process.env.NODE_ENV === 'production' ? '/whod-you-rather' : "", // replace with your GitHub repository name
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/whod-you-rather' : "", // Same as your basePath
   trailingSlash: true,     // Ensure trailing slashes for GitHub Pages to work correctly
   output: 'export',
   images: {
