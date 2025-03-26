bb9cf222a8819f0340b35b0bc828bc7a90f8c5dd
czurmuhl <czurmuhl@gmail.com>
2025-03-25 23:42:02 -0400

    simplify next config

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  distDir: 'out',
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/whod-you-rather' : "", // replace with your GitHub repository name
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/whod-you-rather/' : "", // Same as your basePath
-  trailingSlash: true,     // Ensure trailing slashes for GitHub Pages to work correctly
   images: {
     unoptimized: true,
   }
